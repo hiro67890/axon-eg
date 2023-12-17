db96ad05347003c0c84ab1030ffbfe3317674580
舟木博和 <funakihirokazu@funakihirokazunoMacBook-Air.local>
2023-12-18 00:27:53 +0900

    first commit

---
+++ ProductsService/src/main/kotlin/com/example/productsservice/ProductsServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.productsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductsServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductsServiceApplication>(*args)
+}
